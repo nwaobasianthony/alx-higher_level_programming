7c3e8574ea359872d78dbde97e65e5e3c2257032
nwaobasianthony <41954614+nwaobasianthony@users.noreply.github.com>
2023-10-25 05:52:05 +0100

    task 0 fix

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,5 +1,12 @@
 #!/usr/bin/node
+
+const filename = process.argv[2];
 const fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  console.log(error || content);
+
+fs.readFile(filename, 'utf-8', (error, fileContent) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(fileContent);
+  }
 });
